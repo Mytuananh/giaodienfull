0a382f2c41fc80c3bf19bcdc33b9cb6dd257c3d5
Mytuananh <tuanbenguyencodon11071973@gmail.com>
2023-03-27 02:54:13 +0700

    doing7

---
+++ trang3/trang3.js
@@ -0,0 +1,18 @@
+function openSide() {
+    document.getElementById("my-side-qlct").style.width = "30vh";
+    document.getElementById("main-my-side-qlct").style.marginLeft = "30vh";
+}
+
+function closeSide() {
+    document.getElementById("my-side-qlct").style.width = "0";
+    document.getElementById("main-my-side-qlct").style.marginLeft= "0";
+}
+function openSide1() {
+    document.getElementById("main-my-side-qlct").style.marginRight = "30vh";
+    document.getElementById("my-side-qlct-1").style.width = "30vh";
+}
+
+function closeSide1() {
+    document.getElementById("main-my-side-qlct").style.marginRight= "0";
+    document.getElementById("my-side-qlct-1").style.width = "0";
+}
